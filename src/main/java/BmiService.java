e28fab15c53e2cdd09454243896b2c80f5a0fce4
DENIVON95 <ivonin.denis@mail.ru>
2022-12-18 22:53:32 +0400

    fixes

--- src/main/java/BmiService.java
+++ src/main/java/BmiService.java
@@ -1,12 +1,11 @@
 public class BmiService {
 
     /**
-     *
      * @param height рост в сантиметрах
      * @param weight вес в килограммах
      * @return индекс массы тела
      */
-    public double calculate(double height, double weight){
+    public double calculate(double height, double weight) {
         double convertedHeight = Math.pow(height / 100, 2);
         return weight / convertedHeight;
     }
